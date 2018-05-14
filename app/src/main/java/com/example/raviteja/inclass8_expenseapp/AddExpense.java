fbc91fafaec0df75d0548e75038cd9aef5d7f7f7
RAVI TEJA <ryarlag2@uncc.edu>
2018-05-14 14:04:35 -0400

    Update AddExpense.java

--- app/src/main/java/com/example/raviteja/inclass8_expenseapp/AddExpense.java
+++ app/src/main/java/com/example/raviteja/inclass8_expenseapp/AddExpense.java
@@ -23,13 +23,6 @@ Ravi Teja Yarlagadda
  */
 
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AddExpense.OnFragmentInteractionListener} interface
- * to handle interaction events.
- */
 public class AddExpense extends Fragment {
 
     private OnFragmentInteractionListener mListener;
@@ -127,16 +120,6 @@ public class AddExpense extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void addedExpenses(Expense expense);
